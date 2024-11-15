932b9cdc462dec617621c8a01d6be36e019555ee
Derrmos <denoyer.william@gmail.com>
2024-11-15 01:01:40 +0100

    style: formatting code

--- commands/recruitments/apply.js
+++ commands/recruitments/apply.js
@@ -1,29 +1,37 @@
-const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('apply')
-		.setDescription('Recrutement "The Azgharie"'),
-	async execute(interaction) {
-		const modal = new ModalBuilder()
-			.setCustomId('applyModal')
-			.setTitle('Recrutement "The Azgharie"');
+  data: new SlashCommandBuilder()
+    .setName("apply")
+    .setDescription('Recrutement "The Azgharie"'),
+  async execute(interaction) {
+    const modal = new ModalBuilder()
+      .setCustomId("applyModal")
+      .setTitle('Recrutement "The Azgharie"');
 
-		const cmdrNameInput = new TextInputBuilder()
-			.setCustomId('cmdrNameInput')
-			.setLabel("Votre nom de CMD sur Elite Dangerous")
-			.setStyle(TextInputStyle.Short)
+    const cmdrNameInput = new TextInputBuilder()
+      .setCustomId("cmdrNameInput")
+      .setLabel("Votre nom de CMD sur Elite Dangerous")
+      .setStyle(TextInputStyle.Short);
 
-		const presentationInput = new TextInputBuilder()
-			.setCustomId('presentationInput')
-			.setLabel("Une petite présentation de votre CMD")
-			.setStyle(TextInputStyle.Paragraph)
+    const presentationInput = new TextInputBuilder()
+      .setCustomId("presentationInput")
+      .setLabel("Une petite présentation de votre CMD")
+      .setStyle(TextInputStyle.Paragraph);
 
-		const firstActionRow = new ActionRowBuilder().addComponents(cmdrNameInput);
-		const secondActionRow = new ActionRowBuilder().addComponents(presentationInput);
+    const firstActionRow = new ActionRowBuilder().addComponents(cmdrNameInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(
+      presentationInput
+    );
 
-		modal.addComponents(firstActionRow, secondActionRow);
+    modal.addComponents(firstActionRow, secondActionRow);
 
-		await interaction.showModal(modal);
-	},
+    await interaction.showModal(modal);
+  },
 };
